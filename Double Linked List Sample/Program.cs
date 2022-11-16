7e1e057a0a063cbc34fec0c0fcf6a4d70d3a2735
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-11-16 18:46:19 +0700

    menambahkan kelas Node dan deklarasi variabel

---
+++ Double Linked List Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Linked_List_Sample
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int rollNumber;
+            public string name;
+            public Node next;
+            public Node prev;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
